23beec909cd0425289c6f53d6e2109b7f843946f
andreawork-hub <andrea.vitova@hotmail.com>
2023-12-11 12:12:45 +0100

    initial commit, signup

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="projectname">Vue Book Project</h1>
+  <h1><a href="https://neacodes.com/" rel="home">NEA codes</a></h1>
+  <SignUp/>
+</template>
+
+<script>
+
+import SignUp from './components/SignUp.vue'
+export default {
+  name: 'App',
+components: {
+  SignUp
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.projectname{
+    border: 1px solid darkblue;
+border-radius: 8px 0px;
+background-color: darkblue;
+color: white;
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Sign Up</h1>
+    <div class="signup">
+        <input type="text" placeholder="Enter Name"/>
+        <input type="text" placeholder="Enter Email"/>
+        <input type="password" placeholder="Enter Password"/>
+        <button>Sign Up</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+<style>
+
+.signup input{
+width: 300px;
+height: 30px;
+display: block;
+margin: auto;
+margin-bottom: 30px;
+padding-left: 10px;
+border: 1px solid darkblue;
+}
+.signup button {
+width: 300px;
+height: 30px;
+border: 1px solid darkblue;
+border-radius: 8px 0px;
+background-color: darkblue;
+color: white;
+}
+</style>
