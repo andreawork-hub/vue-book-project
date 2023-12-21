3479a0ac04a8c4a07403ee6f92287d530260f8ed
andreawork-hub <andrea.vitova@hotmail.com>
2023-12-21 20:27:33 +0100

    login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,42 +1,38 @@
 <template>
-    <h1>Sign Up</h1>
-    <div class="signup">
-        <input type="text" v-model="name" placeholder="Enter Name"/>
+    <h1>Login</h1>
+    <div class="login">
         <input type="text" v-model="email" placeholder="Enter Email"/>
         <input type="password" v-model="password" placeholder="Enter Password"/>
-        <button v-on:click="signUp">Sign Up</button>
+        <button v-on:click="login">Login</button>
         <p>
-            <router-link to="/login">Login</router-link>
+            <router-link to="/sign-up">SignUp</router-link>
         </p>
     </div>
 </template>
 <script>
 import axios from 'axios'
 export default {
-    name: 'SignUp', 
-    data () {
+     // eslint-disable-next-line vue/multi-word-component-names
+    name:'Login',
+    data() {
         return {
-            name: '',
             email: '',
             password: ''
-        }
+    }
     }, 
     methods: {
-        async signUp()
-        {
-            let result = await axios.post("http://localhost:3000/users", 
-            {
-                name:this.name, 
-                email:this.email,
-                password:this.password
-            });
-        console.warn(result);
-        if(result.status==201)
+        async login() {
+            let result = await axios.get (
+                `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+            )
+            if(result.status==200 && result.data.length>0)
         {
            
-            localStorage.setItem("user-info",JSON.stringify(result.data))
+            localStorage.setItem("user-info",JSON.stringify(result.data[0]))
             this.$router.push({name:"Home"})
         }
+            console.warn(result)
+
         }
     }, 
     mounted() 
@@ -49,8 +45,7 @@ export default {
 }
 </script>
 <style>
-
-.signup input{
+.login input{
 width: 300px;
 height: 30px;
 display: block;
@@ -59,11 +54,11 @@ margin-bottom: 30px;
 padding-left: 10px;
 border: 1px solid darkblue;
 }
-.signup button {
+.login button {
 width: 300px;
 height: 30px;
 border: 1px solid darkblue;
-border-radius: 16px;
+border-radius: 16px ;
 background-color: darkblue;
 color: white;
 cursor: pointer;
